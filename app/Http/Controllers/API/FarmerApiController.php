38d12246d49a3c470dc1a3f6c22ac4c764e9eae9
syamonetikk <syam.kprakash@onetikk.com>
2022-03-10 18:06:43 +0530

    Farmer section

--- app/Http/Controllers/API/FarmerApiController.php
+++ app/Http/Controllers/API/FarmerApiController.php
@@ -5,9 +5,13 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Farmer;
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Support\Facades\Hash;
 
 class FarmerApiController extends Controller
 {
+    //use HasApiTokens;
+
     public function index()
     {
 
@@ -22,7 +26,51 @@ class FarmerApiController extends Controller
 
     public function store(Request $request)
     {
+        $security_question = [];
+        $security_answer = [];
 
+        $request->validate([
+            'owner_name' => ['required', 'string', 'max:255'],
+            'reg_dob' => ['required'],
+            'reg_age' => ['required'],
+            'reg_gender' => ['required'],
+            'reg_phone' => ['required'],
+            'reg_address' => ['required'],
+            'reg_taluk' => ['required'],
+            'reg_village' => ['required'],
+            'reg_district' => ['required'],
+            'reg_block' => [],
+            'reg_email' => ['required', 'string', 'email', 'max:255', 'unique:farmers'],
+            'reg_pwd' => ['required'],
+            'reg_sec_qs1' => ['required'],
+            'reg_sec_ans1' => ['required'],
+            'reg_sec_qs2' => ['required'],
+            'reg_sec_ans2' => ['required'],
+            'user_type' => ['required'],
+        ]);
+
+        $security_question[] = $request->reg_sec_qs1;
+        $security_question[] = $request->reg_sec_qs2;
+        $security_answer[] = $request->reg_sec_ans1;
+        $security_answer[] = $request->reg_sec_ans2;
+
+        $farmer = Farmer::create([
+            'name' => $request->owner_name,
+            'age' => $request->reg_age,
+            'gender' => $request->reg_gender,
+            'phone' => $request->reg_phone,
+            'address' => $request->reg_address,
+            'taluk' => $request->reg_taluk,
+            'village' => $request->reg_village,
+            'district' => $request->reg_district,
+            'block' => $request->reg_block,
+            'email' => $request->reg_email,
+            'password' => Hash::make($request->password),
+            'security_question' => serialize($security_question),
+            'security_answer' => serialize($security_answer),
+        ]);
+
+        $farmer->save();
     }
 
 }
--- app/Models/Farmer.php
+++ app/Models/Farmer.php
@@ -4,11 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Passport\HasApiTokens;
 
 class Farmer extends Model
 {
-    use HasApiTokens, HasFactory;
 
     /**
      * The attributes that are mass assignable.
